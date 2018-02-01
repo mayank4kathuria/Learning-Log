eca0320b0791efb1dca147bc96b26db543f0699d
Mayank Kathuria <mayank2coolz@gmail.com>
2018-02-01 16:04:31 +0530

    Added inline comments

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    """Default Initialisation Function"""
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
     try:
         from django.core.management import execute_from_command_line
@@ -10,7 +12,8 @@ if __name__ == "__main__":
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
-
+	
+	"""Checks and handles Errors while loading django"""
         try:
             import django
 
